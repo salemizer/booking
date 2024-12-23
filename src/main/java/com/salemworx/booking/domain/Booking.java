1baf45baa98a4febeae7b545d2c107068df669fe
salemizer <ahmed.salem.it@gmail.com>
2024-12-23 14:46:24 +0000

    Changed: Booking domain added equals() impl

--- src/main/java/com/salemworx/booking/domain/Booking.java
+++ src/main/java/com/salemworx/booking/domain/Booking.java
@@ -1,6 +1,7 @@
 package com.salemworx.booking.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -93,6 +94,23 @@ public class Booking {
 
 	public void setStatus(Status status) {
 		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bookingId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Booking other = (Booking) obj;
+		return other.bookingId.equals(bookingId);
 	}
 
 }
