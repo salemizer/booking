e7ac7aa653e9100c57d10e1f0af3ceffbaa160e9
salemizer <ahmed.salem.it@gmail.com>
2025-01-16 15:41:51 +0000

    Refactored: BookingRepository & ItemRepositoryImpl

--- src/main/java/com/salemworx/booking/repository/BookingRepository.java
+++ src/main/java/com/salemworx/booking/repository/BookingRepository.java
@@ -1,18 +1,11 @@
 package com.salemworx.booking.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.salemworx.booking.domain.Booking;
-import com.salemworx.booking.domain.Customer;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-	@Query("select b.status from booking b where b.bookingId = :bookingId")
-	String findStatusByBookingId(@Param("bookingId") Long bookingId);
 }
--- src/main/java/com/salemworx/booking/repository/ItemRepositoryImpl.java
+++ src/main/java/com/salemworx/booking/repository/ItemRepositoryImpl.java
@@ -1,7 +1,12 @@
 package com.salemworx.booking.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.salemworx.booking.domain.Item;
 import jakarta.persistence.EntityManager;
